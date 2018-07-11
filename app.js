0ab30b8702a7914b7633bd4d28b7d097ca33b44b
Shaun Carter <thereal.shauncarter@gmail.com>
2018-07-10 22:00:57 -0600

    heroku upload.

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const app = express();
 
-const coffees = require("./routes/games");
+const games = require("./routes/games");
 
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-app.use("/games", coffees);
+app.use("/games", games);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -20,11 +20,11 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res, next) => {
     res
-    .status(err.status || 500)
-    .json({
-      message: err.message,
-      error: req.app.get("env") === "development" ? err.stack : {}
-    });
+        .status(err.status || 500)
+        .json({
+            message: err.message,
+            error: req.app.get("env") === "development" ? err.stack : {}
+        });
 });
 
 module.exports = app;
